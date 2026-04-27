02008660745d3cf34b8d76deaa657db6d813d95a
Manaswi_Nalwad <84919798+ManaswiN90@users.noreply.github.com>
2026-04-26 21:09:06 -0400

    Add model training function using Linear Regression

--- src/model.py
+++ src/model.py
@@ -1,1 +1,14 @@
 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+def train_model(df, target):
+    X = df.drop(columns=[target])
+    y = df[target]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    return model
